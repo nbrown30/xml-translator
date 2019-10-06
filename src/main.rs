41e19790dcecd500f5a629a53d5dbfdc3c55c124
nbrown30 <2702993+nbrown30@users.noreply.github.com>
2019-10-06 16:26:38 -0400

    :sparkles: :construction: Modifying CData elements

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,23 @@
 extern crate quick_xml;
+extern crate regex;
 
 use quick_xml::Reader;
-use quick_xml::events::{Event, BytesEnd, BytesStart};
+use quick_xml::events::{Event, BytesEnd, BytesStart, BytesText};
 use quick_xml::Writer;
 use std::io::Cursor;
 // use std::iter;
 use std::fs::File;
 use std::io::prelude::*;
 use std::borrow::Cow;
+use std::str::from_utf8;
+use regex::Regex;
 
 fn main() -> std::io::Result<()> {
 
 	let mut reader = Reader::from_file("en.xml").expect("Could not open file to be read");
 	let mut writer = Writer::new(Cursor::new(Vec::new()));
 	let mut buf = Vec::new();
-	let mut count = 0;
+	let merge_field_pattern = Regex::new(r"\{!.+}").unwrap();
 
 	loop {
 		match reader.read_event(&mut buf) {
@@ -24,19 +27,29 @@ fn main() -> std::io::Result<()> {
 				// `e.into_owned()`
 				let mut elem = BytesStart::owned(b"field".to_vec(), "field".len());
 
-				// collect existing attributes
 				elem.extend_attributes(e.attributes().map(|attr| {
-					let mut attr = attr.unwrap();
+						let mut attr = attr.unwrap();
 
-					if attr.key == b"language" {
-						attr.value = Cow::Borrowed(b"es");
-					}
+						if attr.key == b"language" {
+							attr.value = Cow::Borrowed(b"es");
+						}
 
-					attr
-				}));
+						return attr;
+					})
+				);
 
 				// writes the event to the writer
 				assert!(writer.write_event(Event::Start(elem)).is_ok());
+			},
+			Ok(Event::CData(ref e)) => if e.len() != 0 {
+				let cdata_inner_text = from_utf8(e).unwrap();
+
+				if !merge_field_pattern.is_match(cdata_inner_text) {
+					println!("CData({:?})", cdata_inner_text);
+					let elem = BytesText::from_plain(b"Replaced");
+
+					assert!(writer.write_event(Event::CData(elem)).is_ok());
+				}
 			},
 			Ok(Event::End(ref e)) if e.name() == b"field" => {
 				assert!(writer.write_event(Event::End(BytesEnd::borrowed(b"field"))).is_ok());
